2452c599b6c37d154b4b4d3efca18bdcc93efeb0
Vignesh Kumar <vigneshkumar150297@gmail.com>
2017-05-05 13:56:48 +0530

    Create grethanpre.c

---
+++ grethanpre.c
@@ -0,0 +1,30 @@
+//Prints the No. only if it is greater than previous numbers
+Input   : 7
+          1 3 2 7 14 12 17
+Output  : 1 3 7 14 17
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,a[50],i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("%d ",a[0]);
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            if(a[i]>a[j])
+                c++;
+        }
+        if(c==i)
+            printf("%d ",a[i]);
+        c=0;
+    }
+    return 0;
+}
